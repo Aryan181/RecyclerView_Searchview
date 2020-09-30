42573dbcc1cf4b968c63fc4a679b3fabdcd9fcee
Aryan <s-armahindra@lwsd>
2020-09-30 16:28:31 -0700

    Live location extraction

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openMapViewActivity();
+                //openLocationComponentActivity();
             }
         });
     }
@@ -64,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MapViewActivity.class);
         startActivity(intent);
     }
+
+    
 
 
 
